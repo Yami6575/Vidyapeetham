1c61be8736473fd2d8a55890d793fd24b61e8c88
Yami6575 <nikhil.mishra112004@gmail.com>
2024-08-17 14:28:04 +0530

    updated content of rating and otp models

--- server/models/OTP.js
+++ server/models/OTP.js
@@ -15,4 +15,27 @@ const OTPSchema=new mongoose.Schema({
     }
 
 });
+async function sendVerificationEmail(email, otp) {
+
+	try {
+		const mailResponse = await mailSender(
+			email,
+			"Verification Email",
+			emailTemplate(otp)
+		);
+		console.log("Email sent successfully: ", mailResponse.response);
+	} catch (error) {
+		console.log("Error occurred while sending email: ", error);
+		throw error;
+	}
+}
+OTPSchema.pre("save", async function (next) {
+	console.log("New document saved to database");
+
+
+	if (this.isNew) {
+		await sendVerificationEmail(this.email, this.otp);
+	}
+	next();
+});
 module.exports=mongoose.model("OTP",OTPSchema);
